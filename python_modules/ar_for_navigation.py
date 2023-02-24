358aa2c2ffcb71f129bdd4e289c8efc14afc4d95
Mikael Walde <kirillgja@gmail.com>
2023-02-24 13:41:32 +0100

    Robot Position fix. update

--- python_modules/ar_for_navigation.py
+++ python_modules/ar_for_navigation.py
@@ -9,11 +9,19 @@ import numpy as np
 import cv_bridge
 import cv2 as cv
 
-Robots_odometry_msg = Odometry()
+Robots_pose_msg = Odometry()
 bridge = cv_bridge.CvBridge()
 image = None
 pub = rospy.Publisher('ar_nav_target', PoseStamped, queue_size=10)
 odom_pub = rospy.Publisher('ar_base_odom', Odometry, queue_size=10)
+
+# defined parameters under world calibration/placement
+## ar_tag placement
+theta_x = 1.0 #rad
+theta_y = 1.0 #rad
+theta_z = 1.0 #rad
+translation = [1, 2, 3]
+##
 
 def img_from_camera_cb(img):
 	global image
@@ -22,64 +30,56 @@ def img_from_camera_cb(img):
 def ar_pose_marker_cb(msg):	
 	n_of_markers_detected = len(msg.markers)
 	marker = msg.markers
-	target_marker = 3
-
-	world = np.asmatrix([[1, 0, 0, 0],
-                      	 [0, 1, 0, 0],
-                      	 [0, 0, 1, 0],
-                         [0, 0, 0, 1]])
+	target_marker = 3 
 
 	if n_of_markers_detected > 0:
 		for index in range(n_of_markers_detected):
-			p_x = marker[index].pose.pose.position.x
-			p_y = marker[index].pose.pose.position.y
-			p_z = marker[index].pose.pose.position.z
-
-			o_x = marker[index].pose.pose.orientation.x
-			o_y = marker[index].pose.pose.orientation.y
-			o_z = marker[index].pose.pose.orientation.z
-			o_w = marker[index].pose.pose.orientation.w
-			ar_Htm = qt2eul_func(o_x,o_y,o_z,o_w)
-
-			ar_Htm[0, 3] = p_x
-			ar_Htm[1, 3] = p_y
-			ar_Htm[2, 3] = p_z
-
 			if marker[index].id == target_marker:	  
 				rospy.loginfo("Target Frame is Detected!")
+				
 				navigetion_target = marker[index].pose
+				# Find Transormation form Ar tag to Camera
+				p_x = marker[index].pose.pose.position.x
+				p_y = marker[index].pose.pose.position.y
+				p_z = marker[index].pose.pose.position.z
 
-				robot_odom = calulate_robot_pose(ar_Htm, world)
-				x = robot_odom[0, 3]
-				y = robot_odom[1, 3]
-				z = robot_odom[2, 3]
+				o_x = marker[index].pose.pose.orientation.x
+				o_y = marker[index].pose.pose.orientation.y
+				o_z = marker[index].pose.pose.orientation.z
+				o_w = marker[index].pose.pose.orientation.w
+				ar_to_camera_Htm = qt2eul_func(o_x,o_y,o_z,o_w)
 
-				roll, pitch, yaw = tf.euler_from_matrix(robot_odom)
+				ar_to_camera_Htm[0, 3] = p_x
+				ar_to_camera_Htm[1, 3] = p_y
+				ar_to_camera_Htm[2, 3] = p_z
+
+				# Find Camera to World transformation
+				camera_to_world = Camera_to_world_Htm(ar_to_camera_Htm,theta_x,theta_y,theta_z,translation)
+				
+				# Transformating Euler Transofrmation matrix To Quaternion transform
+				x = camera_to_world[0, 3]
+				y = camera_to_world[1, 3]
+				z = camera_to_world[2, 3]
+
+				roll, pitch, yaw = tf.euler_from_matrix(camera_to_world)
 				q_x, q_y, q_z, q_w = tf.quaternion_from_euler(roll,pitch,yaw)
 				
-				Robots_odometry_msg.header.stamp = rospy.Time.now()
-				Robots_odometry_msg.header.frame_id = "odom"
-				Robots_odometry_msg.child_frame_id = "base_footprint"
+				Robots_pose_msg.header.stamp = rospy.Time.now()
+				Robots_pose_msg.header.frame_id = "odom"
+				Robots_pose_msg.child_frame_id = "base_footprint"
 
-				Robots_odometry_msg.pose.pose.position.x = x
-				Robots_odometry_msg.pose.pose.position.y = y
-				Robots_odometry_msg.pose.pose.position.z = z
+				Robots_pose_msg.pose.pose.position.x = x
+				Robots_pose_msg.pose.pose.position.y = y
+				Robots_pose_msg.pose.pose.position.z = z
 				
-				Robots_odometry_msg.pose.pose.orientation.x = q_x 
-				Robots_odometry_msg.pose.pose.orientation.y = q_y
-				Robots_odometry_msg.pose.pose.orientation.z = q_z
-				Robots_odometry_msg.pose.pose.orientation.w = q_w
+				Robots_pose_msg.pose.pose.orientation.x = q_x 
+				Robots_pose_msg.pose.pose.orientation.y = q_y
+				Robots_pose_msg.pose.pose.orientation.z = q_z
+				Robots_pose_msg.pose.pose.orientation.w = q_w
 
 				pub.publish(navigetion_target)	# Navigation Target
-				# This Is not a correct solution for camear to world frame
-				#
-				# I fix
-				#
-				odom_pub.publish(Robots_odometry_msg) # Robot Odometry
-				#
-				#
-				#
-				#
+				odom_pub.publish(Robots_pose_msg) # Robot Odometry
+
 				marker_x_position = marker[index].pose.pose.position.x
 				marker_y_position = marker[index].pose.pose.position.y
 				marker_z_position = marker[index].pose.pose.position.z
@@ -91,20 +91,26 @@ def ar_pose_marker_cb(msg):
 	cv.imshow('frame', image)
 	cv.waitKey(1)
 
+def Camera_to_world_Htm(ar_to_camera_Htm, thetax, thetay, thetaz, translation):
+	camera_to_arHtm = np.linalg.inv(np.asmatrix(ar_to_camera_Htm))
+	ar_to_world_Htm = ar_plasament_calibration(thetax, thetay, thetaz, translation)
+	camera_to_world = camera_to_arHtm * ar_to_world_Htm
+	return camera_to_world
+
+def ar_plasament_calibration(thetax, thetay, thetaz, translation): 
+	ar_Rot = tf.transformations.euler_matrix(thetax, thetay, thetaz, 'xyz')
+	H = np.eye(4)
+	H[:3, :3] = ar_Rot[:3, :3]
+	H[:3, 3] = translation
+	return H  # ar_tag_to_world-tf
 
 def map_coordinate(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
 
 def qt2eul_func(x,y,z,w):
 	quaterion = [x,y,z,w]
 	Htm = tf.quaternion_matrix(quaterion)
 	return Htm
-
-def calulate_robot_pose(Htm_matrix,World): #(Homogeneus transform no translation, Matrix)
-	inverse = np.linalg.inv(np.asmatrix(Htm_matrix))
-	robot_Pose = inverse * World
-	return robot_Pose
 		
 def ar2cv2_coordinate(marker_x_position, marker_y_position, z_distance):
 	z_min = 0.20
