7265ccb872ba7870f385fbdc017245309853bb89
Mikael Walde <kirillgja@gmail.com>
2023-03-21 20:53:04 +0100

    doc. update

--- python_modules/diff_models_Module.py
+++ python_modules/diff_models_Module.py
@@ -26,26 +26,28 @@ def omnidirectional_robotino_model_inverse(vx, vy, omega,d):
     return v1, v2 ,v3
 
 # https://www.itm.uni-stuttgart.de/lehre/praktikum-technische-dynamik/PDFfiles/P03_handout.pdf    
-def omnidirectional_robotino_model_forward(d, L, w1, w2 ,w3,theta):
+def omnidirectional_robotino_model_forward(r, L, w1, w2 ,w3,theta):
     """
-    The function takes in the robot's wheel velocities, the distance between the wheels, the length of
-    the wheels, and the robot's orientation, and returns the robot's linear and angular velocities
+    The function takes in the robot's wheel radius, length between the center of the robot base and the 
+    center of the wheel, and wheel angular velocities, and
+    outputs the robot's linear and angular velocities in the robot's frame
     
-    :param d: distance between the center of the robot and the center of the wheels
-    :param L: distance between the center of the robot and the center of the wheels
+    :param r: radius of the wheels
+    :param L: distance between the center of the robot and the center of the wheel
     :param w1: angular velocity of wheel 1
     :param w2: angular velocity of the middle wheel
     :param w3: angular velocity of the third wheel
-    :param theta: the orientation of the robot in the world frame
-    :return: The linear velocity in the x, y, and theta directions.
+    :param theta: the orientation of the robot
+    :return: the linear velocity in the x, y, and theta directions.
     """
+
     R = np.matrix([[cos(theta), -sin(theta),0],
                    [sin(theta), cos(theta),0],
                    [0,0,1]])
     
-    Jinv = np.matrix([[-d/np.sqrt(3), 0, d/np.sqrt(3)],
-                      [d/3, -2*d/3, d/3],
-                      [d/3*L, d/3*L, d/3*L]])
+    Jinv = np.matrix([[-r/np.sqrt(3), 0, r/np.sqrt(3)],
+                      [r/3, -2*r/3, r/3],
+                      [r/3*L, r/3*L, r/3*L]])
     
     V = np.matrix([[w1],[w2],[w3]])
     lin_vel = R * (Jinv * V)
