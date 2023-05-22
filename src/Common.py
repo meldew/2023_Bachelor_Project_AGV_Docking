debb0ada9eada60e8a87091732a6f304483df1cb
Mikael Walde <kirillgja@gmail.com>
2023-05-22 09:32:36 +0200

    package update

---
+++ src/Common.py
@@ -0,0 +1,12 @@
+
+class Common: 
+    z_min = 0.20
+    z_max = 0.90
+    negX_at_z_min = -0.074
+    posX_at_z_min = 0.080
+    negY_at_z_min = -0.060
+    posY_at_z_min = 0.060
+    negX_at_z_max = -0.34
+    posX_at_z_max = 0.37
+    negX_at_z_max = -0.25
+    posY_at_z_max = 0.25
